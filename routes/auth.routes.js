9e6d7c4d3f876857dcea61ef99c87f7eaf4b19c5
susan <pricelessblinks@gmail.com>
2023-06-07 19:05:13 +0100

    roles

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -17,14 +17,14 @@ module.exports = function (app) {
         .isAlpha()
         .withMessage("First Name can only be alphabets")
         .isLength({ min: 2 })
-        .withMessage("First Minimum of 2 characters"),
+        .withMessage("First Name Minimum of 2 characters"),
       check("last_name")
         .notEmpty()
         .withMessage("Last Name is required")
         .isAlpha()
         .withMessage("Last Name can only be alphabets")
         .isLength({ min: 2 })
-        .withMessage("First Minimum of 2 characters"),
+        .withMessage("Last Name Minimum of 2 characters"),
       check("phone_number")
         .notEmpty()
         .withMessage("Phone Number is required")
