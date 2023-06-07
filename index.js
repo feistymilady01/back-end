8c528ed68f0e5e9a8c39cbbc972407fd356ed1ab
susan <pricelessblinks@gmail.com>
2023-06-07 16:29:47 +0100

    mongoose error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// require("dotenv/config");
+require("dotenv/config");
 require('dotenv').config({ path: '.env' });
 
 const itemRoutes = require("./routes/item.routes");
