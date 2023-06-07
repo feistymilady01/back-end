1bdf9b4496c7c913f9e266b811f0d5c9456ed29b
susan <pricelessblinks@gmail.com>
2023-06-07 16:34:37 +0100

    mongoose error

--- models/user_model.js
+++ models/user_model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
+mongoose.connect('mongodb+srv://pricelessblinks:kWvli4UJegEZRVZq@cluster0.y6txql3.mongodb.net/');
 const Schema = mongoose.Schema;
 
 const user_Schema = new Schema({
