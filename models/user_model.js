09a4bbd83f46da6198562c91e6385f9a221d1782
susan <pricelessblinks@gmail.com>
2023-06-07 17:50:28 +0100

    remove

--- models/user_model.js
+++ models/user_model.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
-require('dotenv').config();
-mongoose.connect('mongodb+srv://pricelessblinks:kWvli4UJegEZRVZq@cluster0.y6txql3.mongodb.net/');
 const Schema = mongoose.Schema;
 
 const user_Schema = new Schema({
